25932058c67406777d4ee73bab1e0c17847ea2f1
CrazyMax <crazy-max@users.noreply.github.com>
2020-12-02 23:13:54 +0100

    Portapps 3.0.0

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 	"path"
 
 	"github.com/portapps/caprine-portable/assets"
-	"github.com/portapps/portapps/v2"
-	"github.com/portapps/portapps/v2/pkg/log"
-	"github.com/portapps/portapps/v2/pkg/shortcut"
-	"github.com/portapps/portapps/v2/pkg/utl"
+	"github.com/portapps/portapps/v3"
+	"github.com/portapps/portapps/v3/pkg/log"
+	"github.com/portapps/portapps/v3/pkg/shortcut"
+	"github.com/portapps/portapps/v3/pkg/utl"
 )
 
 type config struct {
